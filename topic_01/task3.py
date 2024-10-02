e302c9bce11fca7e02b9b99c94a01ab457b6f196
Liza <Liza.rebenok.2006@gmail.com>
2024-10-02 21:24:32 +0300

    add tasks for topic 02

--- topic_01/task3.py
+++ topic_01/task3.py
@@ -1,9 +1,12 @@
 #Завдання №3: написати функцію пошуку дискримінанту
+def descr (a, b, c):
+    result = b**2 - 4*a*c
+    return result
 
 a = float(input("What's a: "))
 b = float(input("What's b: "))
 c = float(input("What's c: "))  
 
-result = b**2 - 4*a*c
 
-print("Result of the discriminant: ", result)
+
+print("Result of the discriminant: ", descr(a, b, c))
